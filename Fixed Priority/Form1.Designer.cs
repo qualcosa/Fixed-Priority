dac723f266535c350038d5e0da7fcdf48de2e164
qualcosa <miglordrop@gmail.com>
2022-01-17 09:44:06 +0200

    Добавьте файлы проекта.

---
+++ Fixed Priority/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace Fixed_Priority
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.About = new System.Windows.Forms.Label();
+            this.NotifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.Context1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Menu1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.Context1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(15, 25);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(99, 20);
+            this.TextBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Имя процесса";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(111, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Приоритет";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(100, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Реал. времени";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(70, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Высокий";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(72, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Обычный";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Location = new System.Drawing.Point(141, 53);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(111, 100);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Каждые";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(6, 65);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(58, 17);
+            this.radioButton6.TabIndex = 3;
+            this.radioButton6.Text = "30 сек";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 42);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(61, 17);
+            this.radioButton5.TabIndex = 2;
+            this.radioButton5.Text = "15 сек.";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(55, 17);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "5 сек.";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(12, 159);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(240, 23);
+            this.Button1.TabIndex = 4;
+            this.Button1.Text = "Задать";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // About
+            // 
+            this.About.AutoSize = true;
+            this.About.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.About.Location = new System.Drawing.Point(116, 26);
+            this.About.Name = "About";
+            this.About.Size = new System.Drawing.Size(17, 18);
+            this.About.TabIndex = 5;
+            this.About.Text = "?";
+            // 
+            // NotifyIcon1
+            // 
+            this.NotifyIcon1.ContextMenuStrip = this.Context1;
+            this.NotifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("NotifyIcon1.Icon")));
+            this.NotifyIcon1.Text = "Fixed Priority";
+            this.NotifyIcon1.Visible = true;
+            // 
+            // Context1
+            // 
+            this.Context1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Menu1});
+            this.Context1.Name = "Context1";
+            this.Context1.Size = new System.Drawing.Size(110, 26);
+            // 
+            // Menu1
+            // 
+            this.Menu1.Name = "Menu1";
+            this.Menu1.Size = new System.Drawing.Size(109, 22);
+            this.Menu1.Text = "Выход";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(264, 193);
+            this.Controls.Add(this.About);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fixed Priority";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.Context1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Label About;
+        private System.Windows.Forms.NotifyIcon NotifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip Context1;
+        private System.Windows.Forms.ToolStripMenuItem Menu1;
+    }
+}
+
---
+++ Fixed Priority/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fixed_Priority
+{
+    public partial class Form1 : Form
+    {
+        readonly bool ru = true;
+        public Form1()
+        {
+            InitializeComponent();
+            if (!CultureInfo.CurrentUICulture.ToString().Contains("ru-RU"))
+            {
+                ru = false;
+                label1.Text = "Process name";
+                groupBox1.Text = "Priority";
+                groupBox2.Text = "Every";
+                Button1.Text = "Set";
+                radioButton1.Text = "Normal";
+                radioButton2.Text = "High";
+                radioButton3.Text = "Realtime";
+                radioButton4.Text = "5 sec";
+                radioButton5.Text = "15 sec";
+                radioButton6.Text = "30 sec";
+            }
+            About.Click += (s, e) => MessageBox.Show($"{(ru ? "Программа задаёт нужный приоритет для всех\nпроцессов программы и не даёт их сбросить пока программа запущена" : "The program sets the desired priority for everyone\nprogram processes and does not allow them to be reset while the program is running")}", "Fixed Priority");
+            Menu1.Click += (s, e) => Close();
+            NotifyIcon1.MouseClick += (s, e) => { if (e.Button == MouseButtons.Left) { Show(); WindowState = FormWindowState.Normal; } };
+            Resize += (s, e) => { if (WindowState == FormWindowState.Minimized) Hide(); };
+        }
+
+        async void Set(ProcessPriorityClass p)
+        {
+            string name = TextBox1.Text;
+            while (Process.GetProcessesByName(name).Length > 0)
+            {
+                Process.GetProcessesByName(name).ToList().ForEach(x => x.PriorityClass = p);
+                await Task.Delay(num);
+            }
+            MessageBox.Show($"{(ru ? $"Процесс с названием {name} не найден.\n\nПрограмма закрывается" : $"Process with the name {name} was not found.\n\nThe program is closing")}", "Fixed Priority", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);
+            Close();
+        }
+
+        int num;
+        void Button1_Click(object sender, System.EventArgs e)
+        {
+            TextBox1.Text = TextBox1.Text.EndsWith(".exe") ? TextBox1.Text.Remove(TextBox1.Text.Length - 4) : TextBox1.Text;
+            TextBox1.Enabled = false;
+            Button1.Enabled = false;
+            groupBox1.Enabled = false;
+            groupBox2.Enabled = false;
+            if (radioButton4.Checked)
+                num = 5000;
+            else if (radioButton5.Checked)
+                num = 15000;
+            else if (radioButton6.Checked)
+                num = 30000;
+            if (radioButton1.Checked)
+                Set(ProcessPriorityClass.Normal);
+            else if (radioButton2.Checked)
+                Set(ProcessPriorityClass.High);
+            else if (radioButton3.Checked)
+                Set(ProcessPriorityClass.RealTime);
+        }
+    }
+}
