a3e796a487c7d3f3990c5c58f512082abc3f6372
qualcosa <miglordrop@gmail.com>
2022-01-17 09:44:06 +0200

    initial commit

---
+++ Fixed Priority/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Fixed Priority")]
+[assembly: AssemblyDescription("Fixed Priority")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("qualcosa")]
+[assembly: AssemblyProduct("Fixed Priority")]
+[assembly: AssemblyCopyright("Copyright © qualcosa")]
+[assembly: AssemblyTrademark("qualcosa")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1390f7bc-2c5c-47ed-a13e-c75b474a8672")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
